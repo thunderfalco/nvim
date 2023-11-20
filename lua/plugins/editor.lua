60c652a654e432afdc6d552d02cdb384e216b606
Shanks <next.songxi@gmail.com>
2023-11-20 09:48:17 +0800

    update

---
+++ lua/plugins/editor.lua
@@ -0,0 +1,101 @@
+return {
+
+  "telescope.nvim",
+  dependencies = {
+    "nvim-telescope/telescope-file-browser.nvim",
+  },
+
+  keys = {
+    {
+      "<leader>fp",
+      function()
+        require("telescope.builtin").find_files({
+          cwd = require("lazy.core.config").options.root,
+        })
+      end,
+      desc = "Find Plugin File",
+    },
+    {
+      ";f",
+      function()
+        local builtin = require("telescope.builtin")
+        builtin.find_files({
+          no_ignore = false,
+          hidden = true,
+        })
+      end,
+    },
+    {
+      ";r",
+      function()
+        local builtin = require("telescope.builtin")
+        builtin.live_grep()
+      end,
+    },
+    {
+      ";t",
+      function()
+        local builtin = require("telescope.builtin")
+        builtin.help_tags()
+      end,
+    },
+    {
+      ";;",
+      function()
+        local builtin = require("telescope.builtin")
+        builtin.resume()
+      end,
+    },
+    {
+      ";e",
+      function()
+        local builtin = require("telescope.builtin")
+        builtin.diagnostics()
+      end,
+    },
+    {
+      ";s",
+      function()
+        local builtin = require("telescope.builtin")
+        builtin.treesitter()
+      end,
+    },
+    {
+      "sf",
+      function()
+        local telescope = require("telescope")
+        local function telescope_buffer_dir()
+          return vim.fn.expand("%:p:h")
+        end
+
+        telescope.extensions.file_browser.file_browser({
+          path = "%:p:h",
+          cwd = telescope_buffer_dir(),
+          respect_gitignore = false,
+          hidden = true,
+          groupd = true,
+          previewer = false,
+          initial_mode = "normal",
+          layout_config = {
+            height = 40,
+          },
+        })
+      end,
+    },
+  },
+  config = function(_, opts)
+    local telescope = require("telescope")
+    local actions = require("telescope.actions")
+    local fb_actions = require("telescope").extensions.file_browser.action
+    opts.defaults = vim.tbl_deep_extend("force", opts.defaults, {
+      wrap_results = true,
+      layout_strategy = "horizontal",
+      layout_config = { prompt_position = "top" },
+      sorting_strategy = "ascending",
+      winblend = 0,
+      mappings = {
+        n = {},
+      },
+    })
+  end,
+}
